9296239767cd4d92aa055f47df938b56643e0f98
mktakuya <takuya.mk96@gmail.com>
2014-04-24 18:28:08 +0900

    Add show entry detail

--- mkdays.py
+++ mkdays.py
@@ -30,9 +30,16 @@ def after_request(response):
 
 @app.route('/')
 def show_entries():
-    cur = g.db.execute('select title, text from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    cur = g.db.execute('select id, title, text from entries order by id desc')
+    entries = [dict(entry_id=row[0], title=row[1], text=row[2]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
+
+@app.route('/entries/<int:entry_id>')
+def show_detail(entry_id):
+    cur = g.db.execute("select id, title, text from entries where id=?", str(entry_id))
+    row = cur.fetchone()
+    entry = dict(entry_id=row[0], title=row[1], text=row[2])
+    return render_template('show_detail.html', entry=entry)
 
 @app.route('/add', methods=['POST'])
 def add_entry():
